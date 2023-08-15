1db223c5dcd8d03c3dff8529f56685ed56adb3fd
Juhaina-Ali <juhaina98.ali@gmail.com>
2023-08-16 00:54:48 +0400

    Initial commit

---
+++ app/src/main/java/com/example/registrationdesign/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.registrationdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinnerLanguages = findViewById(R.id.spinner_language);
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.languages, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerLanguages.setAdapter(adapter);
+    }
+}
